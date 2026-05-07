a54d9a3b347b82fcf5fc3a2e0ec93f015d83abf8
鈴木洵哉 <248467@st.takushoku-u.ac.jp>
2026-05-07 16:23:59 +0900

    第３回

--- practice-js/obj-practice.js
+++ practice-js/obj-practice.js
@@ -16,3 +16,10 @@ let gakka = [
 
 //////////////// ここから下にプログラムを書きたそう!
 
+console.log(campus.address);
+for(const a of campus.buildingD) {
+	console.log(a);
+}
+for(const b of gakka) {
+	console.log(b.name);
+}
