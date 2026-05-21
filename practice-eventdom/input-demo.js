2b0bc407e8e258158b6c06bd7c4c62f61897d850
鈴木洵哉 <248467@st.takushoku-u.ac.jp>
2026-05-21 16:32:33 +0900

    課題提出

---
+++ practice-eventdom/input-demo.js
@@ -0,0 +1,9 @@
+let b = document.querySelector('button#print');
+b.addEventListener('click', greeting);
+function greeting() {
+    let i = document.querySelector('input[name="shimei"]');
+    let shimei = i.value;
+    let aisatu = "こんにちは、" + shimei + "さん";
+    let p = document.querySelector("p#message"); 
+    p.textContent = aisatu;
+}
---
+++ practice-eventdom/tashizan.js
@@ -0,0 +1,10 @@
+let b = document.querySelector("button#calc"); 
+b.addEventListener('click', greeting);
+function greeting() {
+    let lInput = document.querySelector('input[name="left"]');
+    let rInput = document.querySelector('input[name="right"]');
+    let lNum = Number(lInput.value);
+    let rNum = Number(rInput.value);
+    let sum = lNum + rNum;
+    let Answer = document.querySelector("span#answer");
+    Answer.textContent = sum;
